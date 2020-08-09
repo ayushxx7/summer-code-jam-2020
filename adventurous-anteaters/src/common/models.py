600f47f1c594cb17fd0df835cab4242737314e4f
Vadim Suharnikov <vsuharnikov@gmail.com>
2020-08-09 06:05:45 +0300

    Add a custom base user model

--- adventurous-anteaters/src/common/models.py
+++ adventurous-anteaters/src/common/models.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 
 # we use save because auto_now is evil (https://stackoverflow.com/a/1737078)
@@ -16,3 +18,26 @@ class BaseModel(models.Model):
             self.created_at = timezone.now()
         self.updated_at = timezone.now()
         return super().save(*args, **kwargs)
+
+
+class User(AbstractUser, BaseModel):
+    username = None
+    email = None
+    is_staff = None
+    date_joined = None
+    login = models.CharField(
+        _('login'),
+        max_length=150,
+        unique=True,
+        help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        validators=[AbstractUser.username_validator],
+        error_messages={
+            'unique': _("A user with that login already exists."),
+        },
+    )
+
+    USERNAME_FIELD = 'login'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.login
