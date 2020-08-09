b35c5d9bfb102af7d1bd1f8f46f3fcaa3f1545cf
Vadim Suharnikov <vsuharnikov@gmail.com>
2020-08-09 04:00:48 +0300

    Add drf_yasg

--- adventurous-anteaters/src/retro_net/settings.py
+++ adventurous-anteaters/src/retro_net/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_jwt',
     'rest_framework_jwt.blacklist',
+    'drf_yasg',
+
     'common.apps.CommonConfig',
 ]
 
--- adventurous-anteaters/src/retro_net/urls.py
+++ adventurous-anteaters/src/retro_net/urls.py
@@ -13,9 +13,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+#
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Retro Net API",
+        default_version='v1',
+        license=openapi.License(name="MIT"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    re_path(r'^swagger(<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path(r'redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
